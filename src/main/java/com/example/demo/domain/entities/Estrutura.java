921337531dd30775e0471ad7c3afb1920cda820a
Tiago Souza <tiagoadesouza21@gmail.com>
2025-09-19 13:52:15 -0300

    feat(entidades): adiciona algumas entidades

---
+++ src/main/java/com/example/demo/domain/entities/Estrutura.java
@@ -0,0 +1,64 @@
+package com.example.demo.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "estrutura")
+@Table(name = "estruturas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Estrutura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "classificacao_id", nullable = false)
+    private Classificacao classificacao;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column
+    private String complemento;
+
+    @Column
+    private Integer numero;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private Integer cep;
+
+    @ManyToOne
+    @JoinColumn(name = "municipio_id", nullable = false)
+    private Municipio municipio;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime criadoEm;
+
+    @LastModifiedDate
+    private LocalDateTime atualizadoEm;
+
+}
---
+++ src/main/java/com/example/demo/domain/entities/Municipio.java
@@ -0,0 +1,46 @@
+package com.example.demo.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "municipio")
+@Table(name = "municipios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Municipio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "uf_id", nullable = false)
+    private Uf uf;
+
+    @OneToMany(mappedBy = "municipio")
+    private List<Estrutura> estruturas = new ArrayList<>();
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime criadoEm;
+
+    @LastModifiedDate
+    private LocalDateTime atualizadoEm;
+
+}
