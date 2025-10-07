6855f56b2dcd9c2c7f3131d6c8e7a8467cce6beb
Tiago Azevedo <tiagoadesouza21@gmail.com>
2025-10-07 18:07:07 -0300

    fix(validatao): removendo ajustes

--- src/main/java/com/example/demo/domain/validations/strategy/solicitacaocadastro/SolicitacaoCadastroValidationStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.validations.strategy.solicitacaocadastro;
-
-import com.example.demo.domain.dto.solicitacoes.SolicitaCadastroUsuarioDto;
-
-public interface SolicitacaoCadastroValidationStrategy {
-
-    void validar(SolicitaCadastroUsuarioDto solicitaCadastroUsuarioDto);
-}
