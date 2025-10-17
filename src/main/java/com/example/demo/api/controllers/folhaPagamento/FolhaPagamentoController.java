48ffa4a5695d29e44f2addaf71ce0963348fad61
Tiago Azevedo <tiagoadesouza21@gmail.com>
2025-10-16 21:14:19 -0300

    feat(folhapagamento): adiciona documentação swagger e ajusta endpoint

--- src/main/java/com/example/demo/api/controllers/folhaPagamento/FolhaPagamentoController.java
+++ src/main/java/com/example/demo/api/controllers/folhaPagamento/FolhaPagamentoController.java
@@ -3,6 +3,7 @@ package com.example.demo.api.controllers.folhaPagamento;
 import com.example.demo.domain.dto.folhaPagamento.FolhaPagamentoDto;
 import com.example.demo.domain.dto.folhaPagamento.InserirFolhaPagamentoDto;
 import com.example.demo.domain.services.folhapagamento.FolhaPagamentoService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,10 @@ public class FolhaPagamentoController {
         this.folhaPagamentoService = folhaPagamentoService;
     }
 
+    @Operation(
+            summary = "Buscar folha de Pagamento",
+            description = "Retorna a folha de pagamento da estrutura na competencia informada",
+            tags = "Folha de Pagamento (RH)")
     @GetMapping("/buscar")
     public ResponseEntity<FolhaPagamentoDto> buscarFolhaPagamento(
             @RequestParam(name = "estruturaId") UUID estruturaId,
@@ -26,12 +31,16 @@ public class FolhaPagamentoController {
     ){
         FolhaPagamentoDto folhaPagamento = folhaPagamentoService.buscarFolhaPagamento(estruturaId, competenciaId);
         if (folhaPagamento == null){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.noContent().build();
         }
 
         return ResponseEntity.ok(folhaPagamento);
     }
 
+    @Operation(
+            summary = "Inserir valor na folha de Pagamento",
+            description = "Endpoint responsável por inserir valor na folha de pagamento",
+            tags = "Folha de Pagamento (RH)")
     @PostMapping("/inserir-valor")
     public ResponseEntity<Void> inserirValorFolhaPagamento(@Valid @RequestBody InserirFolhaPagamentoDto dto){
         folhaPagamentoService.inserirValorFolhaPagamento(dto);
